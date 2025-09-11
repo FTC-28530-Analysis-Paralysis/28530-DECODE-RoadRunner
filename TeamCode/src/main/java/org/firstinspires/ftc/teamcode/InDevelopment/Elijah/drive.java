112b5c79f5d1467092d29cc983e458f785af9370
Elijah <eporras@alberton.k12.mt.us>
2025-09-11 09:56:53 -0600

    orginized some stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InDevelopment/Elijah/drive.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.InDevelopment.Elijah;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+public class drive {
+
+    private DcMotor
+
+}

c7e4a93589b4a366b409230d2d7c192ac9d4eead
Elijah <eporras@alberton.k12.mt.us>
2025-09-17 10:40:45 -0600

    feat: Add motor control and telemetry to Elijah's OpMode
    This commit introduces basic motor control and telemetry to Elijah's OpMode.
    
    Key changes:
    - Added `hardwareMap` and `telemetry` imports.
    - Defined `DcMotor` variables for all four drive motors.
    - Initialized motors in `runOpMode` using `hardwareMap.get`.
    - Added a telemetry message to indicate initialization status.
    - Added `@TeleOp(name="ELIJAH OPMODE??")` annotation.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InDevelopment/Elijah/drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InDevelopment/Elijah/drive.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.InDevelopment.Elijah;
 
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -8,8 +11,57 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-public class drive {
 
-    private DcMotor
+@TeleOp(name="ELIJAH OPMODE??")
+public class drive {
+    private DcMotor FrontRightDrive;
+    private DcMotor FrontLeftDrive;
+    private DcMotor LeftBackDrive;
+    private DcMotor RightBackDrive;
+
+
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+
+        RightBackDrive = hardwareMap.get(DcMotor.class, "RightBackDrive");
+        LeftBackDrive = hardwareMap.get(DcMotor.class, "LeftBackDrive");
+        FrontLeftDrive = hardwareMap.get(DcMotor.class, "FrontLeftDrive");
+        FrontRightDrive = hardwareMap.get(DcMotor.class, "FrontRightDrive");
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }

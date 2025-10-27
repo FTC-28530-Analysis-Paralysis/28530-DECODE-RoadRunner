80f25b3928e4beaeecf48c06faf37b929d5bec41
nathan.morris <nmorris@alberton.k12.mt.us>
2025-10-27 11:50:31 -0600

    Beginning subsystem code encapsulation - separate files for Intake, Launcher, & MecanumDrive.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware/MecanumDrive.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.RobotHardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class MecanumDrive {
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightBackDrive = null;
+
+
+}
